d1666f17b20e06cf6f396b1ef6794f64b6896071
Barry Wasdell <barrywasdell@gmail.com>
2020-09-09 12:57:19 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+fortune(1) - sample lines from a file
+
+Fortune prints an aphorism chosen at random.
+
+# Sources
+[Jar of fortunes](http://fortunes.cat-v.org/cat-v/)
+
+- [9 front](http://fortunes.cat-v.org/9front/)
+- [cat-v](http://fortunes.cat-v.org/cat-v/)
+- [FreeBSD](http://fortunes.cat-v.org/freebsd/)
+- [FreeBSD (offensive)](http://fortunes.cat-v.org/freebsd/offensive)
+- [Kernelnewbies](http://fortunes.cat-v.org/kernelnewbies/)
+- [OpenBSD](http://fortunes.cat-v.org/openbsd/)
+- [Plan 9](http://fortunes.cat-v.org/plan_9/)
+
+```sh
+wget -O lib/fortunes.index https://9fans.github.io/usr/local/plan9/lib/fortunes
+```
+
+## Other sources
+[Aphorisms concerning aphorisms](https://en.wikiquote.org/wiki/Aphorisms)
+
+# History
+`fortune(6)` first appeared in the Seventh Edition of Unix in 1979,
+written by Ken Arnold.
+
+- [Unix 7th Ed.](http://man.cat-v.org/unix_7th/6/ching)
+- [Unix 8th Ed.](http://man.cat-v.org/unix_8th/6/fortune)
+- [Plan 9 2nd Ed](http://man.cat-v.org/plan_9_2nd_ed/1/)
+- [FreeBSD 1.1](https://svnweb.freebsd.org/base?view=revision&revision=325828)
+- [9 Front (July, 2011)](http://man.cat-v.org/9front/1/)\*
+- [FreeBSD (2017) Removal](https://svnweb.freebsd.org/base?view=revision&revision=325828)\*\*
+- [Debian ?](#)
+
+\* 9Front introduced variants `theo(1)`, `troll(1)` and `bullshit(1)`
+
+\*\* [Revision 325828](https://svnweb.freebsd.org/base?view=revision&revision=325828)
+> Humour is a funny thing. What is funny to one person is not funny to all
+> people. What is insightful to one person is similarly not universal. The
+> fortune datfiles have been around a long time and have undoubtedly amused
+> people but it's time to acknowledge their subjective, and in some cases at
+> least potentially offensive, nature and stop distributing them with the
+> imprimatur of the FreeBSD project.
---
+++ main.go
@@ -0,0 +1,129 @@
+// fortune samples lines from a file
+package main
+
+import (
+	"bufio"
+	"flag"
+	"io"
+	"log"
+	"math/rand"
+	"os"
+)
+
+var (
+	index    = "lib/fortunes.index"
+	fortunes = "lib/fortunes"
+
+	fInfo, ixInfo os.FileInfo
+	fbuf          *buf
+	off           = make([]byte, 4)
+	choice        []byte
+)
+
+const (
+	startBufSize = 2048 // Size of initial allocation for buffer
+)
+
+type buf struct {
+	rdline int   // number of bytes after rdline
+	offset int64 // offset of buffer in file
+	bbuf   []byte
+	rdr    *bufio.Reader
+	fd     *os.File
+}
+
+// Binit initializes a standard size buffer with the open file descriptor passed
+// by the caller.
+func Binit(fd *os.File) *buf {
+	return &buf{
+		rdline: 0,
+		offset: 0,
+		bbuf:   make([]byte, startBufSize),
+		rdr:    bufio.NewReaderSize(fd, startBufSize),
+		fd:     fd,
+	}
+}
+
+// Boffset returns the current file offset.
+func (b *buf) Boffset() uint32 {
+	off, err := b.fd.Seek(0, 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	off += int64(len(b.bbuf))
+	if int64(uint32(off)) != off {
+		log.Fatal("fortune: index >= 4GB")
+	}
+
+	return uint32(off)
+}
+
+// Brdline reads a string from the file associated with b up to and including
+// the first delim character.
+// The delimiter character at the end of the line is not altered. Blinelen
+// returns the length (including the delimiter) of the most recent string
+// returned by Brdline.
+func (b *buf) Brdline(delim byte) ([]byte, int) {
+	p, err := b.rdr.ReadBytes(delim)
+	if err != nil {
+		if err == io.EOF {
+			return nil, 0
+		}
+		log.Fatal(err)
+	}
+	n := copy(b.bbuf, p)
+	b.rdline = n
+	b.offset += int64(n)
+	return p, n
+}
+
+// Blinelen returns the length (including the delimiter) of the most recent
+// string returned by Brdline.
+func (b *buf) Blinelen() int {
+	return b.rdline
+}
+
+// puint32 returns the byte sequence of a little-endian 32-bit unsigned integer
+func puint32(b []byte, v uint32) []byte {
+	_ = b[3] // bounds check
+	b[0] = byte(v)
+	b[1] = byte(v >> 8)
+	b[2] = byte(v >> 16)
+	b[3] = byte(v >> 24)
+	return b
+}
+
+func main() {
+	log.SetFlags(0)
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) > 0 {
+		log.Printf("Misfortune!\n")
+		os.Exit(2)
+	}
+
+	f, err := os.Open(fortunes)
+	if err != nil {
+		log.Fatalf("Misfortune!\n%s", err)
+	}
+	defer f.Close()
+
+	fbuf = Binit(f)
+	rand.Seed(int64(os.Getpid()))
+	for i := 1; ; i++ {
+		offs := fbuf.Boffset()
+		p, n := fbuf.Brdline('\n')
+		if n == 0 {
+			break
+		}
+		p = p[:fbuf.Blinelen()-1]
+		off = puint32(off, offs)
+		if rand.Int31()%int32(i) == 0 {
+			choice = p
+		}
+	}
+
+	log.Printf("%s", choice)
+	os.Exit(0)
+}
